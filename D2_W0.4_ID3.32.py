ef9e324ea43ffd5f1d3c2b500fb9678d78cb1832
subber <jihaibiao012@163.com>
2022-11-22 21:05:37 +0800

    专门为M2-psychopy在1024*768屏幕上做的数据文件

---
+++ D2_W0.4_ID3.32.py
@@ -0,0 +1,43 @@
+# -*- coding = utf-8 -*-
+# @Time : 2022/8/25 21:13
+# @Author : 计海彪
+# @File : Number.py
+# @Software : PyCharm
+import os,csv,random,math,numpy
+
+
+if not os.path.exists('number_file'):
+    os.mkdir('number_file')
+data_file = open('number_file/'+'D2_W0.4_point_contrast_psychopy'+'.csv','w',encoding='UTF-8',newline='')
+writer = csv.writer(data_file)
+writer.writerow(["Pos_Current","WIDTH","distance","ID"])
+
+
+screen_x = 1024
+screen_y = 768
+w = 0.4  #圆的直径（width）
+base = 100 #倍数
+d = 2 #距离为10base
+ID = math.log(2 * d/w, 2)
+
+#生成点的范围
+x_min = w/2
+x_max = screen_x/base -w +x_min
+y_min = w/2
+y_max = screen_y/base -w +y_min
+
+i = 1
+x_pre = screen_x/base
+y_pre = 0
+writer.writerow([[round(x_pre * base - screen_x/2, 0), round( y_pre * base - screen_y/2, 0)], w * base, d *base ,round (ID,3)])
+while i <= 32:
+    x = round(random.uniform(x_pre-d,x_pre+d),3)
+    if x_min < x < x_max:
+        y_p = round(y_pre + math.sqrt( numpy.square(d)-numpy.square(x- x_pre)),3)
+        y_n = round(y_pre - math.sqrt(numpy.square(d) -numpy.square(x - x_pre)),3)
+        y = random.choice([y_n,y_p])#一正一负，，随机挑
+        if y_min < y < y_max:
+            writer.writerow([[round(x * base - screen_x/2, 0), round( y * base - screen_y/2, 0)], w * base, d *base ,round (ID,3)])  # 转换成psychopy里面的坐标
+            x_pre = x
+            y_pre = y
+            i += 1
--- D6_W1.2_ID3.32.py
+++ D6_W1.2_ID3.32.py
@@ -8,33 +8,36 @@ import os,csv,random,math,numpy
 
 if not os.path.exists('number_file'):
     os.mkdir('number_file')
-data_file = open('number_file/'+'D4_W0.8_point_contrast_psychopy'+'.csv','w',encoding='UTF-8',newline='')
+data_file = open('number_file/'+'D6_W1.2_point_contrast_psychopy'+'.csv','w',encoding='UTF-8',newline='')
 writer = csv.writer(data_file)
 writer.writerow(["Pos_Current","WIDTH","distance","ID"])
 
-
-w = 0.8  #圆的直径（width）
+screen_x = 1024
+screen_y = 768
+w = 1.2  #圆的直径（width）
 base = 100 #倍数
-d = 4 #距离为10base
+d = 6 #距离为10base
 ID = math.log(2 * d/w, 2)
+
 
 #生成点的范围
 x_min = w/2
-x_max = 1920/base -w +x_min
+x_max = screen_x/base -w +x_min
 y_min = w/2
-y_max = 1080/base -w +y_min
+y_max = screen_y/base -w +y_min
 
 i = 1
-x_pre = 9
-y_pre = 5
-while i <= 31:
+x_pre = screen_x/base
+y_pre = 0
+writer.writerow([[round(x_pre * base - screen_x/2, 0), round( y_pre * base - screen_y/2, 0)], w * base, d *base ,round (ID,3)])
+while i <= 32:
     x = round(random.uniform(x_pre-d,x_pre+d),3)
     if x_min < x < x_max:
-        y_p = round(y_pre + math.sqrt( numpy.square(d)-numpy.square(x-x_pre)),3)
+        y_p = round(y_pre + math.sqrt( numpy.square(d)-numpy.square(x- x_pre)),3)
         y_n = round(y_pre - math.sqrt(numpy.square(d) -numpy.square(x - x_pre)),3)
         y = random.choice([y_n,y_p])#一正一负，，随机挑
         if y_min < y < y_max:
-            writer.writerow([[round(x * base - 960, 0), round( 540 - y * base, 0)], w * base, d *base ,round (ID,3)])  # 转换成psychopy里面的坐标
+            writer.writerow([[round(x * base - screen_x/2, 0), round( y * base - screen_y/2, 0)], w * base, d *base ,round (ID,3)])  # 转换成psychopy里面的坐标
             x_pre = x
             y_pre = y
             i += 1
